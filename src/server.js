bfcf1c26b8ff92bfc2e485a244207a673ca08ec1
lucaslira <lucasliraone@gmail.com>
2023-06-27 16:15:24 -0300

    query param lesson

--- src/server.js
+++ src/server.js
@@ -2,13 +2,19 @@ const express = require("express");
 
 const app = express();
 app.get("/message/:id/:user", (request, response) => {
-    const {id, user} = request.params
+    const {id, user} = request.params;
 
     response.send(`
-    Esse é o id: ${id}
-    Esse é o user: ${user}
-    `)
+        Esse é o id: ${id}.
+        Esse é o user: ${user}.
+    `);
+})
+app.get("/users", (request, response) => {
+    const { page, limit } = request.query;
 
+    response.send(`
+        Página: ${page}. Mostrar: ${limit}
+    `);
 })
 
 const PORT = 3333;
