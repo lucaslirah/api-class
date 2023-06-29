62cd4a19cf2843adb3e57ad5605d887fa302717d
lucaslira <lucasliraone@gmail.com>
2023-06-29 10:50:10 -0300

    organizing the project structure - separate responsabilities

--- src/server.js
+++ src/server.js
@@ -3,9 +3,8 @@ const express = require("express");
 const routes = require("./routes");
 
 const app = express();
-app.post("/users", (request, response) => {
-    response.send(`Você chamou o POST.`);
-})
+app.use(express.json());
+app.use(routes);
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
