1bfdfe7d28825a31c593699aaa17d5300836bc00
lucaslira <lucasliraone@gmail.com>
2023-06-29 10:41:16 -0300

    organizing project structure - separate responsibilities

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+const {Router} = require("express");
+
+const usersRouters = require("./users.routes");
+
+const routes =  Router();
+
+routes.use("/users", usersRouters);
+
+module.exports = routes;
