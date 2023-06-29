8a50976bad136faf9e824f866f3cec1f9b3ca86e
lucaslira <lucasliraone@gmail.com>
2023-06-28 21:09:34 -0300

    body params lesson - using json format in response

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
+
 app.post("/users", (request, response) => {
-    response.send(`Você chamou o POST.`);
-})
+    
+    const { name, email, password } = request.body;
+
+    response.json({ name, email, password });
+});
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
