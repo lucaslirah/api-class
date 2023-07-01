96a368729580f44a1aa0c550d1e7545e81e97f48
lucaslira <lucasliraone@gmail.com>
2023-06-30 21:31:29 -0300

    UserController.js file created

---
+++ src/controllers/UserController.js
@@ -0,0 +1,13 @@
+class UserController {
+    /* 
+        - `index` - GET to list multiple records.
+        - `show` - GET to display a specific record.
+        - `create` - POST to create a record.
+        - `update` - PUT to update a record.
+        - `delete` - DELETE to remove a record.  
+    */
+
+
+}
+
+module.exports = UserController;
