e8e738e033188b8fd16a637b04dd79443e05b93d
LucasLiraSilva <lucasliraone@gmail.com>
2023-08-14 19:56:27 -0300

    review Controllers: Users Controllers

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -16,3 +16,14 @@
 // }
 
 // module.exports = UserController;
+
+class UsersController {
+
+    create( request, response ){
+        const { name, email, password } = request.body;
+
+        response.json({ name, email, password });
+    };
+};
+
+module.exports = UsersController;
