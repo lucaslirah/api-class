3fd5436e77f72c987b2176546ce99c19ca59bab0
LucasLiraSilva <lucasliraone@gmail.com>
2023-08-16 21:03:56 -0300

    Handling exceptions, installing express-async-errors

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -16,11 +16,16 @@
 // }
 
 // module.exports = UserController;
+const AppError = require("../utils/AppError");
 
 class UsersController {
 
     create( request, response ){
         const { name, email, password } = request.body;
+
+        if(!name){
+            throw new AppError("Name is required!");
+        };
 
         response.json({ name, email, password });
     };
--- src/server.js
+++ src/server.js
@@ -8,15 +8,32 @@
 
 // const PORT = 3333;
 // app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
-
+// require('express-async-errors')
+const AppError = require("../src/utils/AppError")
 const express = require('express');
-
 const routes = require("./routes/index.js"); //by default JS uses index.js file, so is not necessary put it.
 
 const app = express();
-app.use(express.json());
 
+app.use(express.json());
 app.use(routes);
+app.use( (error, request, response, next) => {
+
+    if(error instanceof AppError){
+        return response.status(400).json({
+            "status": "Error",
+            "message": error.message
+        });
+    };
+
+    console.error(error);
+
+    return response.status(500).json({
+        "status": "Error",
+        "message": "Internal Error"
+    });
+
+});
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
