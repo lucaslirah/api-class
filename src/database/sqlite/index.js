1b1c690bd2ebac8f53efc78ee7eb620a66f9546b
lucaslira <lucasliraone@gmail.com>
2023-08-18 16:25:07 -0300

    database redone

--- src/database/sqlite/index.js
+++ src/database/sqlite/index.js
@@ -1,10 +1,10 @@
 const sqlite = require("sqlite");
 const sqlite3 = require("sqlite3");
-const path = require("path")
+const path = require("path");
 
-async function sqliteConnection(){
+async function sqliteConnection() {
     const database = await sqlite.open({
-        filename: path.resolve(__dirname, "..", "database.db"),
+        filename: path(__dirname, "..", "database.db"),
         driver: sqlite3.Database
     });
 
