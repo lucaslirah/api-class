03a7caee132f2b59301cb5f72cac4984a00e04e3
LucasLiraSilva <lucasliraone@gmail.com>
2023-08-20 15:44:04 -0300

    express-async-errors added

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@
 // const PORT = 3333;
 // app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
 // require('express-async-errors')
+require("express-async-errors");
 const AppError = require("../src/utils/AppError")
 const express = require('express');
 const routes = require("./routes/index.js"); //by default JS uses index.js file, so is not necessary put it.
