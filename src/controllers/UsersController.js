c12f6c7b0b442ded890cfa0813e833cc50ddb7c9
lucaslira <lucasliraone@gmail.com>
2023-08-22 10:41:35 -0300

    updating password using bcrypt's compare method

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -16,7 +16,7 @@
 // }
 
 // module.exports = UserController;
-const { hash } = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError");
 const sqliteConnection = require("../database/sqlite");
 
@@ -40,7 +40,7 @@ class UsersController {
         return response.status(201).json();
     };
     async update( request, response ){
-        const { name, email } = request.body;
+        const { name, email, password, old_password } = request.body;
         const { id } = request.params;
 
         const database = await sqliteConnection();
@@ -64,14 +64,30 @@ class UsersController {
         user.name = name;
         user.email = email;
 
+        if(password && !old_password){
+            throw new AppError("You need to enter the old password to set the new password.");
+        };
+
+        
+        if(password && old_password){
+            const checkOldPassword = await compare(old_password, user.password);
+
+            if(!checkOldPassword){
+                throw new AppError("The old password does not match.");
+            };
+
+            user.password = await hash (password, 8);
+        };
+
         await database.run(`
             UPDATE users SET
                 name = ?,
                 email = ?,
+                password = ?,
                 updated_at = ?
                 WHERE id = ?
                 `,
-            [ user.name, user.email, new Date(), id ]
+            [ user.name, user.email, user.password, new Date(), id ]
         );
 
         return response.json(); //here the status is not necessary, by default response.json() will return it
