e1dab3d3733d93ff548459488f692cdbe287a357
lucaslira <lucasliraone@gmail.com>
2023-09-27 12:53:36 -0300

    Validating user

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,10 +1,15 @@
 const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
 
 class SessionsController{
     async create({ request, response}){
         const { email, password } = request.body;
 
         const user = await knex("users").where({email}).first();
+
+        if(!user){
+            throw new Error("Incorrect email and/or password.", 401)
+        }
 
         return response.json(user);
     }
