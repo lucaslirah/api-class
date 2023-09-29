38d767181e60b10b963fed53aa4509457780e0a7
LucasLiraSilva <lucasliraone@gmail.com>
2023-09-29 20:33:07 -0300

    validating password with compare method of bcryptjs

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,15 +1,21 @@
 const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
+const { compare } = require("bcryptjs");
 
 class SessionsController{
     async create(request, response){
         const { email, password } = request.body;
-        console.log(email)
 
         const user = await knex("users").where({email}).first();
 
         if(!user){
             throw new AppError("Incorrect email and/or password.", 401)
+        }
+
+        const passwordMatched = await compare(password, user.password);
+
+        if(!passwordMatched){
+            throw new AppError("Incorrect email and/or password.", 401);
         }
 
         return response.json(user);
