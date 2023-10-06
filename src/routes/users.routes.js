816a00740f90ebab02f9665401373d7f40155924
LucasLiraSilva <lucasliraone@gmail.com>
2023-10-06 19:55:55 -0300

    Loading images - code refactored

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -8,7 +8,10 @@
 
 // module.exports = usersRoutes;
 
-const { Router } = require('express');
+const { Router } = require("express");
+
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 const UsersController = require("../controllers/UsersController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
@@ -16,17 +19,23 @@ const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 const usersRoutes = Router();
 
 // function myMiddleware(request, response, next){
-
-//     if(!request.body.isAdmin){
-//         return response.json({ message: "user unauthorized"});
-//     };
-
-//     next();
-// }
-
+    
+    //     if(!request.body.isAdmin){
+        //         return response.json({ message: "user unauthorized"});
+        //     };
+        
+        //     next();
+        // }
+        
 const usersController = new UsersController();
+
+const upload = multer(uploadConfig.MULTER);
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthenticated, usersController.update);
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (req, res) => {
+    console.log(req.file.filename);
+    res.json({"pode p": "á"});
+});
 
 module.exports = usersRoutes;
