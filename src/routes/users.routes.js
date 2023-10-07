5548d89245485bb879b5f8eaa732ee225324c41e
LucasLiraSilva <lucasliraone@gmail.com>
2023-10-06 21:02:44 -0300

    saving image in the data base

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -14,6 +14,9 @@ const multer = require("multer");
 const uploadConfig = require("../configs/upload");
 
 const UsersController = require("../controllers/UsersController");
+
+const UsersAvatarController = require("../controllers/UsersAvatarController");
+
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const usersRoutes = Router();
@@ -29,13 +32,12 @@ const usersRoutes = Router();
         
 const usersController = new UsersController();
 
+const usersAvatarController = new UsersAvatarController();
+
 const upload = multer(uploadConfig.MULTER);
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthenticated, usersController.update);
-usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (req, res) => {
-    console.log(req.file.filename);
-    res.json({"pode p": "á"});
-});
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), usersAvatarController.update);
 
 module.exports = usersRoutes;
