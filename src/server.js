e01d170abbe10ed9e4dd877fb9d12b83ff1774bf
LucasLiraSilva <lucasliraone@gmail.com>
2023-10-09 20:56:39 -0300

    cors library installed - server.js refactored

--- src/server.js
+++ src/server.js
@@ -9,30 +9,34 @@
 // const PORT = 3333;
 // app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
 // require('express-async-errors')
+
 require("express-async-errors");
-const migrationsRun = require("./database/sqlite/migrations")
-const AppError = require("../src/utils/AppError")
+const migrationsRun = require("./database/sqlite/migrations");
+const AppError = require("../src/utils/AppError");
+const uploadConfig = require("./configs/upload");
+
+const cors = require("cors");
 const express = require('express');
 const routes = require("./routes/index.js"); //by default JS uses index.js file, so is not necessary put it.
-const uploadConfig = require("./configs/upload");
 
 migrationsRun();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
-app.use(express.json());
 app.use(routes);
-app.use( (error, request, response, next) => {
-    if(error instanceof AppError){
-        return response.status(error.statusCode).json({
-            "status": "Error",
-            "message": error.message
+app.use( (err, request, response, next) => {
+    if(err instanceof AppError){
+        return response.status(err.statusCode).json({
+            "status": "error",
+            "message": err.message
         });
     };
 
-    console.error(error);
+    console.error(err);
 
     return response.status(500).json({
         "status": "Error",
